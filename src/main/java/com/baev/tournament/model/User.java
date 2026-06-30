ed36e147c19615df7712c207524e9969dafc5c3a
dbaev00 <dbaev00@gmail.com>
2026-06-30 13:11:53 +0300

    проверка работы репозитория пользователей с базой данных Добавлен CommandLineRunner в класс TournamentManagerApplication. Написан тест для метода save() (сохранение пользователя и проверка на уникальность логина). Написан тест для метода findByUsername() (успешный поиск существующего пользователя). Написан тест для метода findByUsername() (возврат null для несуществующего пользователя).

--- src/main/java/com/baev/tournament/model/User.java
+++ src/main/java/com/baev/tournament/model/User.java
@@ -28,7 +28,7 @@ public class User {
     public void setPassword(String password){this.password = password;}
 
     public String getEmail(){return email;}
-    public void setEmail(String password){this.email = email;}
+    public void setEmail(String email){this.email = email;}
 
     public Role getRole(){return role;}
     public void setRole(Role role){this.role = role;}
