8411a10acd2a84fd576d2df6551aa6b6ccccbdeb
dbaev00 <dbaev00@gmail.com>
2026-07-04 14:43:10 +0300

    Добавил Lombok

--- src/main/java/com/baev/tournament/model/User.java
+++ src/main/java/com/baev/tournament/model/User.java
@@ -1,7 +1,13 @@
 package com.baev.tournament.model;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
 
+import java.util.Objects;
+@Data//геттеры, сеттеры, toString, equals и hashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private Long id;
     private String username;
@@ -9,37 +15,12 @@ public class User {
     private String email;
     private Role role;
 
-    public User(String username, String password, String email, Role role){
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.role = role;
-    }
 
-    public User() {}
-
-    public Long getId(){return id;}
-    public void setId(Long id){ this.id = id;}
-
-    public String getUsername(){ return username;}
-    public void setUsername(String username){ this.username = username;}
-
-    public String getPassword(){return password;}
-    public void setPassword(String password){this.password = password;}
-
-    public String getEmail(){return email;}
-    public void setEmail(String email){this.email = email;}
-
-    public Role getRole(){return role;}
-    public void setRole(Role role){this.role = role;}
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User)o;
-        return id != 0 && this.id.equals(user.id);
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        User user = (User)o;
+//        return id != 0 && this.id.equals(user.id);
+//    }
 }
-
-
